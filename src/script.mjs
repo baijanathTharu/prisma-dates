61af683804fbf1f278bfaa92c1e26fa69a43cd71
baijanathTharu <bnchaudhary.tharu@gmail.com>
2023-01-16 14:46:03 +0545

    add data fetching in script

--- src/script.mjs
+++ src/script.mjs
@@ -1,8 +1,31 @@
 import { execa } from 'execa';
+import fetch from 'node-fetch';
+
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!res.ok) {
+    return {
+      message: 'FETCH_ERROR',
+      status: res.status,
+    };
+  }
+  const data = await res.json();
+
+  return data;
+}
 
 async function myScript() {
   const { stdout } = await execa('echo', ['from the script file']);
   console.log(stdout);
+
+  const d = await fetchUsers();
+  console.log('users', d);
 }
 
 myScript()
